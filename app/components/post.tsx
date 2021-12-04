3f8a387390818efa529e9140326ce8acf7178e24
Tim Ermilov <yamalight@gmail.com>
2021-12-04 14:02:22 +0100

    Correctly handle posts with text

--- app/components/post.tsx
+++ app/components/post.tsx
@@ -1,9 +1,23 @@
 import { formatDistanceToNow } from 'date-fns';
 import { useMemo } from 'react';
+import ReactMarkdown from 'react-markdown';
 import Awards from './awards.js';
+
+// FIXME: update react-markdown to v7 once Remix
+// correctly supports importing of ES Modules
+
+const getImage = (post) => {
+  if (post.selftext?.length > 0) {
+    return;
+  }
+  const images = post.preview?.images?.[0];
+  const sourceImage = images?.source?.url;
+  return sourceImage?.replace(/&amp;/g, '&');
+};
 
 export default function Post({ post }) {
   const date = useMemo(() => new Date(post.created_utc * 1000), [post]);
+  const image = useMemo(() => getImage(post), [post]);
 
   return (
     <div className="flex flex-col overflow-hidden rounded-lg shadow-lg border border-gray-100">
@@ -24,52 +38,19 @@ export default function Post({ post }) {
         </time>
         <Awards awards={post.all_awardings} />
       </div>
-      {post.thumbnail !== 'self' && (
-        <div className="flex-shrink-0">
-          <img
-            className="object-cover w-full h-48"
-            src={post.thumbnail}
-            alt=""
-          />
+      <p className="p-2 text-2xl font-semibold text-neutral-600 mb-2">
+        <a href={`https://reddit.com/${post.permalink}`}>{post.title}</a>
+      </p>
+      {image && (
+        <div className="flex flex-shrink-0 items-center justify-center">
+          <img className="object-contain" src={image} alt={post.title} />
         </div>
       )}
-      <div className="flex flex-col justify-between flex-1 p-6 bg-white">
-        <div className="flex-1">
-          <a href="#" className="block mt-2">
-            <p className="text-xl font-semibold text-neutral-600">
-              <a href={`https://reddit.com/${post.permalink}`}>{post.title}</a>
-            </p>
-            <p className="mt-3 text-base text-gray-500">
-              {' '}
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Architecto accusantium praesentium eius, ut atque fuga culpa,
-              similique sequi cum eos quis dolorum.{' '}
-            </p>
-          </a>
-        </div>
-        <div className="flex items-center mt-6">
-          <div className="flex-shrink-0">
-            <a href={`https://reddit.com/user/${post.author_fullname}`}>
-              <span className="sr-only">{post.author_fullname}</span>
-            </a>
-          </div>
-          <div className="ml-3">
-            <p className="text-sm font-medium text-neutral-600">
-              <a
-                href={`https://reddit.com/user/${post.author_fullname}`}
-                className="hover:underline"
-              >
-                {post.author_fullname}
-              </a>
-            </p>
-            <div className="flex space-x-1 text-sm text-gray-500">
-              <time dateTime={new Date(post.created_utc).toISOString()}>
-                {new Date(post.created_utc).toLocaleDateString()}
-              </time>
-            </div>
-          </div>
-        </div>
+      <div className="text-base text-gray-500 px-4 mt-2 bg-white prose max-w-max">
+        <ReactMarkdown>{post.selftext}</ReactMarkdown>
       </div>
+
+      {/* debuggin json */}
       <details className="mt-6">
         <summary className="text-sm font-medium text-neutral-600">
           Post JSON

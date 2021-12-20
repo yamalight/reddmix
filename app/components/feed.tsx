de8ba85f699e8be58b2ef21b514a781c9b823279
Tim Ermilov <yamalight@gmail.com>
2021-12-20 16:53:11 +0100

    Filter out duplicate posts on "load more" manually

--- app/components/feed.tsx
+++ app/components/feed.tsx
@@ -2,10 +2,19 @@ import { useEffect, useRef, useState } from 'react';
 import { useIntersectionObserver } from '~/hooks/useIntersectionObserver.js';
 import Post from './post/index.js';
 
-export default function Feed({ initialPosts, initialAfter, subreddit = '' }) {
+export default function Feed({
+  initialPosts,
+  initialAfter,
+  subreddit = '',
+}: {
+  initialPosts: Array<any>;
+  initialAfter: string;
+  subreddit: string;
+}) {
   const loadMoreRef = useRef();
-  const [posts, setPosts] = useState([]);
-  const [after, setAfter] = useState(null);
+  const existingPostsRef = useRef<{ [postId: string]: boolean }>({});
+  const [posts, setPosts] = useState<any>([]);
+  const [after, setAfter] = useState<string | null>(null);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -25,7 +34,11 @@ export default function Feed({ initialPosts, initialAfter, subreddit = '' }) {
       body: JSON.stringify({ after, subreddit }),
     }).then((r) => r.json());
 
-    setPosts([...posts].concat(res.posts));
+    setPosts(
+      [...posts].concat(
+        res.posts.filter((p) => !existingPostsRef.current[p.id])
+      )
+    );
     setAfter(res.after);
     setLoading(false);
   };
@@ -38,12 +51,22 @@ export default function Feed({ initialPosts, initialAfter, subreddit = '' }) {
     <>
       {posts && (
         <div className="flex flex-col p-4 gap-8 items-center">
-          {posts.map((post) => (
-            <Post key={post.id} post={post} />
-          ))}
-          {loading && <div>Loading..</div>}
+          {posts.map((post) => {
+            // mark current post as in-feed (used during pagination)
+            existingPostsRef.current[post.id] = true;
+            // return post render
+            return <Post key={post.id} post={post} />;
+          })}
+          {loading && (
+            <div className="text-gray-900 dark:text-gray-100">Loading..</div>
+          )}
           {!loading && after && (
-            <button ref={loadMoreRef} type="submit" onClick={loadMore}>
+            <button
+              ref={loadMoreRef}
+              type="submit"
+              onClick={() => loadMore(true)}
+              className="text-gray-900 dark:text-gray-100"
+            >
               Load more
             </button>
           )}
